b3067ba0638b252cede29e4e48e71f22b44b9a13
mfairless <mfairless@Michael-Fairlesss-MacBook-Pro.local>
2015-10-24 15:29:11 -0400

    Updated

--- plot3.R
+++ plot3.R
@@ -18,7 +18,7 @@ ggp <- ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
   geom_bar(stat="identity") +
   theme_bw() + guides(fill=FALSE)+
   facet_grid(.~type,scales = "free",space="free") + 
-  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(x="Year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
   labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
 
 print(ggp)
